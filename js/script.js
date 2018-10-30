765cd31d26ba1a5cf4a5b707416400286087f054
mrtwin <42963686+mrtwin@users.noreply.github.com>
2018-10-30 18:45:26 +0500

    add animate

--- js/script.js
+++ js/script.js
@@ -20,7 +20,14 @@ $(document).ready(function(){
 		    });
 	    return false;
  	});
-
+	 //плавная прокрутка
+	$(function(){
+        $("a[href^='#']").click(function(){
+                var _href = $(this).attr("href");
+                $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+                return false;
+        });
+	});
 
 	
 });	
